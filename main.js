57aa813eb7d91eca85ffb633271579bff51fb319
betse-1 <betsegaws@gmail.com>
2022-07-21 10:00:27 +0300

    Add popup for my works section, fix linter error and and content dynamically

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let isClicked = false;
+let isClicked2 = false;
 // eslint-disable-next-line no-unused-vars
 function display() {
   if (!isClicked) {
@@ -14,4 +15,192 @@ function display() {
     document.getElementById('option2').style.display = 'none';
     isClicked = false;
   }
+}
+const myProjects = [
+  {
+    id: 0,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+  {
+    id: 1,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+  {
+    id: 2,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+  {
+    id: 3,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+  {
+    id: 4,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+  {
+    id: 5,
+    name: 'Multi-Post Stories Gain+Glory',
+    title: 'Keeping track of hundreds of components',
+    image: '',
+    technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'html'],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveView: '#',
+    sourcecode: '#',
+  },
+
+];
+
+// eslint-disable-next-line no-unused-vars
+function modalDisplay() {
+  if (isClicked2 !== true) {
+    document.getElementById('modal-section').style.display = 'block';
+    isClicked2 = true;
+  } else {
+    document.getElementById('modal-section').style.display = 'none';
+    isClicked2 = false;
+  }
+}
+const modalRender = (id) => {
+  const modal = document.getElementById('modal-id');
+  const wrapper = document.createElement('div');
+  wrapper.className = 'modal-content';
+  let element = '';
+  element = `
+<div class="close-btn-wrap">
+<img id="close-btn" class="close-btn1" src="image/Disable2.png" onclick="modalDisplay()" alt="close button">
+</div>
+<div class="snapshot">
+<img id="snapshot" src="image/Snapshoot-portfolio.png"  alt="snapshot">
+</div>
+<div class="close-btn">
+<img id="close-btn2" class="close-btn2" src="image/Snapshoot-mob-portfolio.png" onclick="modalDisplay()" alt="Mobile version snapshot">
+</div>
+<div class="modal-btns">
+  <a href= '${myProjects[id].liveView}'>
+  <button class="see-project-mbutton1" type="button">
+  <div class='btn-modal'>
+    <p>See Live</p>
+    <img src="image/Icon-export.png" alt="" id="liveimg" />
+    </div>
+  </button>
+  </a>
+  <a href='${myProjects[id].liveView}'>
+  <button class="see-project-mbutton2" type="button">
+    <div class='btn-modal'>
+    <p>See Source</p>
+    <img src="image/Icon-gitHub.png" alt="" id="gitimg" />
+    </div>
+  </button>
+  </a>
+</div>
+<div class="modal-head">
+  <h2>${myProjects[id].title}</h2>
+    <ul class="skill-list">
+      <li>Codekit</li>
+      <li>Github</li>
+      <li>javascript</li>
+      <li id="mob-list">Bootsrap</li>
+      <li id="mob-list">Terminal</li>
+      <li id="mob-list">codepen</li>
+    </ul>
+</div>
+<div class="modal-text">
+  <p>${myProjects[id].description}</p>
+</div>
+<div class="mob-modal-btns">
+  <a href= '${myProjects[id].liveView}'>
+  <button class="see-project-mbutton1" type="button">
+  <div class='btn-modal'>
+    <p>See Live</p>
+    <img src="image/Icon-export.png" alt="" id="liveimg" />
+    </div>
+  </button>
+  </a>
+  <a href='${myProjects[id].sourcecode}'>
+  <button class="see-project-mbutton2" type="button">
+    <div class='btn-modal'>
+    <p>See Source</p>
+    <img src="image/Icon-gitHub.png" alt="" id="gitimg" />
+    </div>
+  </button>
+  </a>
+</div>
+ `;
+
+  modal.appendChild(wrapper);
+  wrapper.innerHTML = element;
+  const close = document.getElementById('close-btn');
+  const close2 = document.getElementById('close-btn2');
+
+  close.addEventListener('click', () => {
+    wrapper.innerHTML = '';
+    modal.removeChild(wrapper);
+  });
+  close2.addEventListener('click', () => {
+    wrapper.innerHTML = '';
+    modal.removeChild(wrapper);
+  });
+};
+
+const grid = document.getElementById('main-wrap');
+const test = document.createElement('div');
+test.className = 'grid-container';
+let gridContainor = '';
+for (let i = 0; i < myProjects.length; i += 1) {
+  gridContainor = `
+  <div class="my-work-card">
+  <div class="card-image"></div>
+  <div class="my-works-text">
+    <h2>${myProjects[i].name}</h2>
+    <ul class="built-with">
+     ${
+  myProjects[i].technologies.map((tech) => `<li>${tech}</li>`).join('')
+}
+    </ul>
+    <button type="button" onclick="modalDisplay()"  class="work-button" id="my-work-button1" data-id="${
+  myProjects[i].id
+}">See Project</button>
+  </div>
+  </div>
+
+  `;
+  grid.appendChild(test);
+  test.innerHTML += gridContainor;
+
+  const mybutton = document.querySelectorAll('.work-button');
+  mybutton.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      modalRender(e.target.dataset.id);
+    });
+  });
 }
